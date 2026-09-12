e17558f75a066fb9b2a66f5809153f45048a8770
tenmane <235837458+tenmane@users.noreply.github.com>
2026-09-12 20:52:39 +0400

    feat: add player name to Player constructor

--- src/control/player.js
+++ src/control/player.js
@@ -2,7 +2,8 @@ import { Gameboard } from "./gameboard.js";
 import { Ship, shipSizes } from "./ship.js";
 
 export class Player {
-  constructor(isComputer = false) {
+  constructor(name, isComputer = false) {
+    this.name = name;
     this.gameBoard = new Gameboard();
     this.ships = shipSizes.map((size) => {
       return new Ship(size);

6d556c0b4395fcfa35ee6b4fab281359a89157f9
tenmane <235837458+tenmane@users.noreply.github.com>
2026-09-17 13:42:38 +0400

    chore: update battle log to reflect current turn

--- src/DOM/events.js
+++ src/DOM/events.js
@@ -1,20 +1,28 @@
 import { Player } from "../control/player.js";
-import { renderBoard } from "./render.js";
+import {
+  freezeGrid,
+  pauseGrid,
+  renderBoard,
+  resumeGrid,
+  unfreezeGrid,
+} from "./render.js";
 import { renderHit } from "./render.js";
+
+const firstGrid = document.querySelector(".first-grid");
+const secondGrid = document.querySelector(".second-grid");
+const log = document.querySelector(".battle-log");
 
 const playerForm = document.querySelector(".player-form");
 playerForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const playerOne = new Player();
   const bot = new Player(true);
-  const firstGrid = document.querySelector(".first-grid");
-  const secondGrid = document.querySelector(".second-grid");
 
   playerOne.placeAllShipsRandomly();
   bot.placeAllShipsRandomly();
   renderBoard(playerOne.gameBoard, firstGrid, false);
   renderBoard(bot.gameBoard, secondGrid, true);
-
+  freezeGrid(firstGrid);
   const boxes = document.querySelectorAll(".box");
   boxes.forEach((box) => {
     box.addEventListener("click", () => {
@@ -23,20 +31,36 @@ playerForm.addEventListener("submit", (e) => {
         box.dataset.col,
       ]);
       renderHit(box.dataset.row, box.dataset.col, secondGrid, playerAttack);
+      unfreezeGrid(firstGrid);
+
       if (bot.gameBoard.allSunk()) {
-        console.log("You Won!");
+        log.textContent = "You Won!";
+        freezeGrid(firstGrid);
+        freezeGrid(secondGrid);
+        return;
       }
-      
-      let botAttack;
-      do {
-        let row = Math.floor(Math.random() * 10);
-        let col = Math.floor(Math.random() * 10);
-        botAttack = playerOne.gameBoard.receiveAttack([row, col]);
-        renderHit(row, col, firstGrid, botAttack);
-        if (playerOne.gameBoard.allSunk()) {
-          console.log("You Lost!");
-        }
-      } while (botAttack === "Duplicate Position!");
+      pauseGrid(firstGrid);
+      freezeGrid(secondGrid);
+      log.textContent = "Enemy Turn";
+      setTimeout(() => {
+        let botAttack;
+        do {
+          let row = Math.floor(Math.random() * 10);
+          let col = Math.floor(Math.random() * 10);
+          botAttack = playerOne.gameBoard.receiveAttack([row, col]);
+          renderHit(row, col, firstGrid, botAttack);
+
+          if (playerOne.gameBoard.allSunk()) {
+            log.textContent = "You Lost!";
+            return;
+          }
+        } while (botAttack === "Duplicate Position!");
+
+        unfreezeGrid(secondGrid);
+        resumeGrid(firstGrid);
+        freezeGrid(firstGrid);
+        log.textContent = "Your Turn";
+      }, 1000);
     });
   });
 });
